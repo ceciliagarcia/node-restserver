4ffd89a6f00b0858797c072d0694fbb73ee3ef56
Cecilia <ceciliagarciagon92@gmail.com>
2020-05-15 17:45:35 +0200

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cecilia:EJP5Tx2YKK3EE9iX@cluster0-6vacb.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
